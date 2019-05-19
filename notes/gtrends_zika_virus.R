d7196f5a36f8234896af4c6c8630960420574848
Everton Portela <everton_portela@yahoo.com.br>
2019-05-19 16:54:01 -0300

    Ajustes nos dados do Google Trends para agrupar os dados por mês, ano e UF, para uso em conjunto com os dados do arquivo zika-data/notes/baseZIKA/lista_dados_sage.csv

--- notes/gtrends_zika_virus.R
+++ notes/gtrends_zika_virus.R
@@ -1,6 +1,7 @@
 library (gtrendsR)
 library(lubridate)
 library(stringr)
+library(dplyr)
 
 city_trend = data.frame(location=character(),
                         hits=integer(),
@@ -17,16 +18,15 @@ channel='web'
 geo="BR"
 
 # coleta os dados do google trends por mes, partindo de 01/01/2015, ate 31/12/2016
-month_list=seq(ymd(20150101), ymd(20161201), by ="month")
-# month_list=seq(ymd(20150101), ymd(20150601), by ="month")
+month_list=seq(ymd(20160101), ymd(20161201), by ="month")
 for (i in seq_along(month_list)){
   print(paste0("Processando mes/ano ", month(month_list[i]), "/", year(month_list[i]), "..."))
   s_date = month_list[i]
   l_date = ceiling_date(s_date, "month") - days(1)
   month_year = paste0(str_pad(month(s_date), 2, pad = "0"), "-", year(s_date))
 
-  # tmp_city_trend = gtrends(keywords, gprop=channel, geo=geo, time=paste(s_date, l_date), low_search_volume=TRUE)$interest_by_city
   tmp_trend = gtrends(keywords, gprop=channel, geo=geo, time=paste(s_date, l_date), low_search_volume=TRUE)
+
   # city data
   tmp_city_trend = tmp_trend$interest_by_city
   if (typeof(tmp_city_trend) == "NULL"){
@@ -45,5 +45,16 @@ for (i in seq_along(month_list)){
   }
 }
 
-write.csv(city_trend,'city_trend.csv', row.names = FALSE)
-write.csv(region_trend,'region_trend.csv', row.names = FALSE)
+# fill NA
+city_trend[is.na(city_trend)] = 0
+region_trend[is.na(region_trend)] = 0
+
+# siglas de UF
+gtrendsR_states = read.csv2(file = 'gtrendsR_states.csv', header = TRUE)
+
+# join
+region_trend = merge(region_trend, gtrendsR_states, by="location")
+
+# save to csv
+# write.csv2(city_trend,'city_trend.csv', row.names = FALSE, fileEncoding = "CP1252")
+write.csv2(region_trend,'region_trend.csv', row.names = FALSE, fileEncoding = "CP1252")
